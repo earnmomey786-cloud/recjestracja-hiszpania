56cb5281b530c8735689dba7ba994182b5b1ccb9
Truong Huynh My Van <VAN231A370373@st.vhu.edu.vn>
2025-09-26 20:54:37 +0200

    Clean up project: remove unused files and folders

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Tus opciones de configuración de Next.js van aquí si las tienes.
-  // Por ahora, un objeto vacío es perfectamente válido.
-};
-
-export default nextConfig;
-
